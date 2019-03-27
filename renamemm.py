b5b2c1e568da1ae2d3d674cb8ad43f7fc97ed636
Linar <46284951+xForgot10x@users.noreply.github.com>
2019-03-28 00:11:43 +0500

    Code improvements
    No functional changes whatsoever. Improved the code visually. Did my best to comply with PEP 8:
    -renamed a function and some variables;
    -added comments for clarity, removed inline comments;
    -limited the line length.
    
    Also started using formatted strings prefixed with "F" rather than followed by ".format()" - saves space.

--- renamemm.py
+++ renamemm.py
@@ -2,49 +2,59 @@ import os
 from tinytag import TinyTag
 
 path = input("Enter the desired folder path: ")
-try :
+# Change directory or quit if it cannot be done
+try:
     os.chdir(path)
-except :
+except:
     print("Invalid path")
     quit()
 
-Extensions =  frozenset([".mp3", ".flac", ".m4a", ".mp4", ".m4b", ".wav", ".wma", ".ogg", ".oga", ".opus"])
-ReservedChars = frozenset(["<", ">", ":", "/", "\\", "|", "?", "*", "\""])
+# Supported file extensions
+extensions =  frozenset([".mp3", ".flac", ".m4a", ".mp4", ".m4b",
+                         ".wav", ".wma", ".ogg", ".oga", ".opus"])
+# List of charachers not allowed in file names under Windows
+reserved_chars = frozenset(["<", ">", ":", "/", "\\", "|", "?", "*", "\""])
 
-def WinSafeName(trackname) :
+def win_safe_name(trackname):
     """Edit the track name so as the resulting file name
     would be allowed in Windows"""
+    # In order: strip the whitespace at both ends, replace a trailing
+    # full stop by an underscore. Replace any forbidden characters by
+    # underscores.
     trackname = trackname.strip()
-    if trackname.endswith(".") :
+    if trackname.endswith("."):
         trackname = trackname[:len(trackname) - 1] + "_"
-    for chr in trackname :
-        if chr in ReservedChars :
+    for chr in trackname:
+        if chr in reserved_chars:
             trackname = trackname.replace(chr, "_")
     return trackname
 
-for filename in os.listdir() :
-    if os.path.isfile(filename) :
+# Iterate through the files in set folder
+for filename in os.listdir():
+    if os.path.isfile(filename):
         file, ext = os.path.splitext(filename)
-        if ext in Extensions :
+        # Discard files with unsupported extensions
+        if ext in extensions:
             tag=TinyTag.get(filename)
-            tag.track = str(int(tag.track)) #getting rid of leading zeroes in track numbers
-            if tag.disc is not None :
-                tag.disc = str(int(tag.disc)) #same for discs
-            tag.title = WinSafeName(tag.title)
-            if tag.disc_total is None or int(tag.disc_total) == 1 :
-                if int(tag.track) < 10 :
-                    os.rename(filename, "0{} - {}{}".format(tag.track, tag.title, ext))
-                else :
-                    os.rename(filename, "{} - {}{}".format(tag.track, tag.title, ext))
-            else :
-                if int(tag.track) < 10 :
-                    os.rename(filename, "{}.0{} - {}{}".format(tag.disc, tag.track, tag.title, ext))
-                else :
-                    os.rename(filename, "{}.{} - {}{}".format(tag.disc, tag.track, tag.title, ext))
+            # Get rid of leading zeroes in track numbers
+            tag.track = str(int(tag.track))
+            # Get rid of leading zeroes in disc numbers
+            if tag.disc is not None:
+                tag.disc = str(int(tag.disc))
+            tag.title = win_safe_name(tag.title)
+            # Rename files for single-disc albums
+            if tag.disc_total is None or int(tag.disc_total) == 1:
+                if int(tag.track) < 10:
+                    os.rename(filename, F"0{tag.track} - {tag.title}{ext}")
+                else:
+                    os.rename(filename, F"{tag.track} - {tag.title}{ext}")
+            # Rename files for multiple-disc albums
+            else:
+                if int(tag.track) < 10:
+                    os.rename(filename,
+                              F"{tag.disc}.0{tag.track} - {tag.title}{ext}")
+                else:
+                    os.rename(filename,
+                              F"{tag.disc}.{tag.track} - {tag.title}{ext}")
 
 print("Done")
-                    
-#Only tested in Windows. Theoretically should work on other platforms too
-#TinyTag library is required to run this script
-#Written using tinytag 1.0.0
-#https://pypi.org/project/tinytag/
